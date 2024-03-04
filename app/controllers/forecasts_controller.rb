7438ce1b9ac8c7f3c4326c399eff23a0ebd15092
Luis Carlos González Hernández <mlyxqyqe4@mozmail.com>
2024-03-03 20:58:57 -0600

    Adds forecast controller.
    It gets the location from reservamos api, then uses that information to
    get forecast for a week and save it to a cookie.
    
    Adds the views to show the list of cities.

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,4 +1,46 @@
 class ForecastsController < ApplicationController
   def index
+    init_cookie
+    @cities = JSON.parse cookies[:cities]
+  end
+
+  def create
+    init_cookie
+    place = search_place(query: forecast_params).call
+    forecast = get_forecast(lat: place['lat'].to_f, lon: place['lon'].to_f).call
+    cities = cities_from_cookie
+    cities << city(name: place['display'], forecast: forecast)
+    cookies[:cities] = cities.to_json
+
+    redirect_to root_path
+  end
+
+  private
+
+  def init_cookie
+    cookies[:cities] = '[]' if cookies[:cities].nil?
+  end
+
+  def cities_from_cookie
+    JSON.parse cookies[:cities]
+  end
+
+  def city(name: '', forecast: [])
+    {
+      name: name,
+      forecast: forecast
+    }
+  end
+
+  def forecast_params
+    params.require(:query)
+  end
+
+  def search_place(query: '')
+    Reservamos::SearchPlaceService.new(query)
+  end
+
+  def get_forecast(lat: 0, lon: 0)
+    OpenWeatherMap::GetForecastService.new(lat: lat, lon: lon)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'forecasts#index'
-  resources :cities, only: [:index]
+  resources :forecasts, only: [:index, :create]
 
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
